2722e6100529e1e1cbfe196f8569cfa61db5224b
WiseCraftMedia <wisecraftmedia@gmail.com>
2024-08-21 22:48:18 -0400

    ready for render

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 let express = require("express")
 let { MongoClient, ObjectId } = require("mongodb")
+require("dotenv").config()
+let sanitizeHTML = require("sanitize-html")
 
 let app = express()
 let db
 
-require("dotenv").config()
-
-const mongoUri = process.env.MONGO_URI
-
 app.use(express.static("public"))
+const mongoUri = process.env.MONGO_URI
 
 async function go() {
   let client = new MongoClient(mongoUri)
-  try {
-    await client.connect()
-    db = client.db()
-    app.listen(3000)
-  } catch (e) {
-    console.log(e)
-  }
+  await client.connect()
+  db = client.db()
+  app.listen(3000)
 }
 
 go()
@@ -27,94 +22,70 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(__dirname))
 
-app.get("/", async (req, res) => {
-  const punches = await db.collection("punches").find().toArray()
-  res.send(`<html lang='en'>
+function userPass(req, res, next) {
+  res.set("WWW-Authenticate", 'Basic realm="simple todo app"')
+  console.log(req.headers.authorization)
+  if (req.headers.authorization == "Basic bGVhcm46amF2YXNjcmlwdA==") {
+    next()
+  } else {
+    res.status(401).send("auth required")
+  }
+}
+
+app.use(userPass)
+
+app.get("/", userPass, async function (req, res) {
+  const items = await db.collection("items").find().toArray()
+  res.send(`<!DOCTYPE html>
+  <html>
   <head>
-    <meta charset='UTF-8' />
-    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    <link rel='stylesheet' href='simple.css' />
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple To-Do App</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   </head>
   <body>
-    <div class='page'>
-      <h1>Punchy!</h1>
-      <div class='punch'>
-        <form action='/punch' method='POST'>
-          <label><input autofocus name='punch' /><button>Add new item</button></label>
-        </form>
-      </div>
-    </div>
-    <div class='listbox'>
-      <ul class='list'>
-        ${punches
-          .map(punch => {
-            return `<li class="fist">
-          <span class='single-punch'>${punch.text}</span>
-            <div>
-                      <button data-id='${punch._id}' class="edit">Edit</button><button data-id='${punch._id}' class="delete">Delete</button>
-
-            </div>
-          </li>`
-          })
-          .join("")}
-      </ul>
-    </div>
-        <div class="test">
-      <button class="test-local" data-stringId='${
-        punches._id
-      }'>Check Local</button><button class="test-network" data-stringId="${
-    punches._id
-  }">Check...Network?</button>
-    </div>
-
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="/browser.js"></script>
-   </body>
-</html>
-`)
+  <div class="container">
+  <h1 class="display-4 text-center py-1">To-Do App!</h1>
+  
+  <div class="jumbotron p-3 shadow-sm">
+  <form id="create-form" action="/create-item" method="POST">
+  <div class="d-flex align-items-center">
+  <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+  <button class="btn btn-primary">Add New Item</button>
+  </div>
+  </form>
+  </div>
+  
+  <ul id="item-list" class="list-group pb-5">
+  </ul>
+  
+  </div>
+  <script>
+  let items = ${JSON.stringify(items)}
+  </script>
+  
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="/browser.js"></script>
+  </body>
+  </html>`)
 })
 
-//
-
-app.post("/punch", async (req, res) => {
-  await db.collection("punches").insertOne({ text: req.body.punch })
-  res.redirect("/")
+app.post("/create-item", async function (req, res) {
+  let safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: [] })
+  const info = await db.collection("items").insertOne({ text: safeText })
+  res.json({ _id: info.insertedId, text: req.body.text })
 })
 
-app.post("/test", (req, res) => {
-  res.send(`Test route working
-    <a href="localhost:3000">Home</a>
-    `)
+app.post("/update-item", async function (req, res) {
+  let safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: [] })
+  await db
+    .collection("items")
+    .findOneAndUpdate({ _id: new ObjectId(req.body.id) }, { $set: { text: safeText } })
+  res.send("Success")
 })
 
-app.post("/change-punch", async (req, res) => {
-  try {
-    console.log("Received data:", req.body)
-    const result = await db
-      .collection("punches")
-      .findOneAndUpdate({ _id: new ObjectId(req.body.id) }, { $set: { text: req.body.punch } })
-    console.log("Update result:", result)
-
-    // if (result.matchedCount === 0) {
-    //   return res.status(404).send("Document not found")
-    // }
-
-    res.send("Edits approved")
-  } catch (err) {
-    console.error("Error during database operation:", err)
-    res.status(500).send("Error updating document")
-  }
-})
-
-app.post("/feint-punch", async (req, res) => {
-  try {
-    console.log("Received data:", req.body)
-    const result = await db.collection("punches").deleteOne({ _id: new ObjectId(req.body.id) })
-    console.log("Update result:", result)
-    res.redirect("/")
-  } catch (err) {
-    console.error("Error during database operation:", err)
-    res.status(500).send("Error updating document")
-  }
+app.post("/delete-item", async function (req, res) {
+  await db.collection("items").deleteOne({ _id: new ObjectId(req.body.id) })
+  res.send("Success")
 })
